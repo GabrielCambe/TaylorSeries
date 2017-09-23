23953bb2b6dfc741adf618274340b286baf237f5
GabrielCambe <30998470+GabrielCambe@users.noreply.github.com>
2017-09-23 11:42:02 -0300

    Create function.h

---
+++ function.h
@@ -0,0 +1,19 @@
+#ifndef __FUN__
+#define __FUN__
+
+#include <math.h>
+#include <stdlib.h> //para abs
+#include <stdio.h>
+#define PI 3.14159265358979323846
+
+//constante que para o loop dos somatórios
+#define DIF 0.0000000001
+
+double my_exp(double x);
+//double my_sin(double x);
+//double my_cos(double x);
+//double my_arcsin(double x);
+//double my_arccos(double x);
+
+
+#endif
